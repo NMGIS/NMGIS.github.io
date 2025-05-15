ead40ab5a85007da018f49ce3f2f2cd06efed7bf
Nevin <nevinmcintyregis@gmail.com>
2025-05-15 15:14:57 -0700

    BoardGames sort, new games

--- src/components/BoardGames.jsx
+++ src/components/BoardGames.jsx
@@ -1,14 +1,15 @@
+import React, { useRef, useState, useEffect } from 'react';
 import { boardGames } from '../data/boardGameData';
 import RadarChart from './RadarChart';
-import React, { useRef, useState, useEffect } from 'react';
-
-
 
 const BoardGames = () => {
   const boardGamesRef = useRef(null);
   const [showBackToTop, setShowBackToTop] = useState(false);
-  const refs = useRef(boardGames.map(() => React.createRef()));
   const [selectedIndex, setSelectedIndex] = useState('');
+
+  // Sort games by score
+  const sortedGames = [...boardGames].sort((a, b) => b.score - a.score);
+  const refs = useRef(sortedGames.map(() => React.createRef()));
 
   useEffect(() => {
     const handleScroll = () => {
@@ -27,18 +28,19 @@ const BoardGames = () => {
         <h1 style={{ textAlign: 'center' }}>Board Game Ratings</h1>
         <br />
         <p>A score of 1 is lowest, 5 is highest. Here's what each category means:</p>
-        <br/>
+        <br />
         <ul>
           <li>Replayability: How often you’ll play it again</li>
           <li>Complexity: Rules depth and mechanical challenge</li>
-          <li>Difficulty: Learning curve or ease of access</li>
+          <li>Difficulty: Learning curve or ease of access (lower scores = very difficult or easy)</li>
           <li>Parts/Portability: Setup, cleanup, and travel friendliness</li>
           <li>Expansions: Quality or necessity of expansions</li>
           <li>Affordability: Value for cost</li>
           <li>Art Style & Theme: Subjective; doesn’t affect overall rating</li>
         </ul>
-        <br/>
-        <p><select
+        <br />
+        <p style={{ textAlign: 'center' }}>
+          <select
             value={selectedIndex}
             onChange={(e) => {
               const index = e.target.value;
@@ -49,20 +51,20 @@ const BoardGames = () => {
             }}
           >
             <option value="">Select a Game...</option>
-            {boardGames
-              .map((game, index) => ({ ...game, index }))
-              .sort((a, b) => b.score - a.score)
-              .map((game) => (
-                <option key={game.index} value={game.index}>
-                  {game.name} – {game.score}
-                </option>
-              ))}
-          </select></p>
-        <br/>
-        {boardGames.map((game, idx) => (
+            {sortedGames.map((game, index) => (
+              <option key={index} value={index}>
+                {game.name} – {game.score}
+              </option>
+            ))}
+          </select>
+        </p>
+        <br />
+        {sortedGames.map((game, idx) => (
           <div className="board-game" key={idx} ref={refs.current[idx]} style={{ marginBottom: '2rem' }}>
             <h3>
-              <a href={game.url} target="_blank" rel="noopener noreferrer">{game.name}</a> – {game.score}
+              <a href={game.url} target="_blank" rel="noopener noreferrer">
+                {game.name}
+              </a> – {game.score}
             </h3>
             <ul>
               {game.notes.map((note, i) => (
@@ -89,7 +91,7 @@ const BoardGames = () => {
             cursor: 'pointer',
             boxShadow: '0 4px 8px rgba(0,0,0,0.3)',
             zIndex: 1000,
-            fontWeight: 'bold'
+            fontWeight: 'bold',
           }}
         >
           Back to Top
--- src/data/boardGameData.js
+++ src/data/boardGameData.js
@@ -1,85 +1,78 @@
-export const boardGames = [
+const boardGames = [
   {
     name: "Blood Rage",
-    score: 4.0,
     url: "https://www.cmon.com/products/blood-rage/",
     notes: [
       "Mystics of Midgard expansion is worth it. The Kickstarter Exclusive expansion is expensive and in limited supply, but I personally think it's worth it if you enjoy the game.",
       "A digital version is available on Board Game Arena. There is also a official digital version on steam which has a low player count.",
-      
+
     ],
     youtube: ["https://youtu.be/EkvmTFUGiyQ?si=lFsU9ZMBV-XDj1FV"],
     ratings: [5.0, 4.0, 4.0, 3.5, 4.0, 3.5, 5.0]
   },
   {
     name: "Root",
-    score: 3.75,
     url: "https://ledergames.com/products/root-a-game-of-woodland-might-and-right",
     notes: [
       "One of the best asymmetric board games on the market.",
       "Very steep learning curve. Difficult for new players to learn.",
       "I would recommend every expansion with the exception of the Clockwork expansion.",
       "A digital version is available on IOS, Android, and Steam which I highly recommend when learning the game.",
-      
+
     ],
     youtube: ["https://youtu.be/U9zFqRQ11qI?si=DZAcEuz_2SqytU1d"],
     ratings: [5.0, 5.0, 2.0, 3.0, 4.0, 3.5, 5.0]
   },
   {
     name: "Overboss",
-    score: 4.0,
     url: "https://www.brotherwisegames.com/overboss",
     notes: [
       "Winning requires some degree of luck.",
       "Strategizing does improve your chances of winning.",
       "Tallying final score is time consuming and could be difficult for some players.",
-      
+
     ],
     youtube: ["https://www.youtube.com/watch?v=k3aLaVfnfxw"],
     ratings: [5.0, 3.5, 4.5, 3.0, 3.5, 4.5, 2.5]
   },
   {
     name: "Air, Land, & Sea",
-    score: 4.1,
     url: "https://www.arcanewonders.com/product/air-land-sea/",
     notes: [
       "Amazing portable card game! My go-to road trip or pub game.",
       "2 players only",
       "Both games share the same core mechanics and can be mixed together.",
       "Check out the Critters at War  versions before you buy. It's a cute cartoon version of the game with identical rules.",
-      
+
     ],
     youtube: ["https://youtu.be/OrnKNBuH_Z8?si=n2odlnfR58dVQSaU"],
     ratings: [4.5, 3.7, 4.5, 5.0, 2.0, 5.0, 3.0]
   },
   {
     name: "Boss Monster",
-    score: 3.8,
     url: "https://www.brotherwisegames.com/boss-monster",
     notes: [
       "My Nieces and Nephews' all-time favorite board game.",
       "All of the expansions are worth it, but may not be used in every game.",
       "Slightly difficult to learn/execute the turn order.",
-      
+
     ],
     youtube: ["https://youtu.be/d9FR-Z3ExWs?si=pg4FvhVyMGZO-JaK"],
     ratings: [3.6, 3.5, 3.5, 3.7, 4.2, 4.5, 5.0]
   },
   {
     name: "Here to Slay",
-    score: 3.3,
     url: "",
     notes: [
       "My Nieces and Nephews' 2nd favorite board game.",
       "You can create your own custom Party Leader on the website",
-      
+
     ],
     youtube: ["https://youtu.be/38DkmTankr8?si=tpouKqvwr23XX3IR"],
     ratings: [3.0, 2.5, 3.8, 4.0, 3.0, 3.5, 3.0]
   },
   {
     name: "Star Realms",
-    score: 3.2,
     url: "https://www.starrealms.com/",
     notes: [
       "Both games share the core mechanics, but have different themes.",
@@ -92,148 +85,135 @@ export const boardGames = [
   },
   {
     name: "Minecraft",
-    score: 3.6,
     url: "https://www.ravensburger.us/discover/games/minecraft/index.html",
     notes: [
       "Piece heavy, with a long setup time",
       "Farmer's Market Expansion is worth it, but increases the amount of pieces and setup time",
-      
+
     ],
     youtube: ["https://youtu.be/OnblaPBorjY?si=sjo8w3RzdLEALqXO"],
     ratings: [3.7, 4.2, 3.8, 3.0, 3.0, 4.0, 5.0]
   },
   {
     name: "Dumb Ways to Die",
-    score: 2.6,
     url: "https://dwtd.spinmaster.com/",
     notes: [
-      
+
     ],
     youtube: ["https://youtu.be/S_SLdDcr30Q?si=JPb5artoeMi9bGYM"],
     ratings: [2.0, 1.7, 3.6, 4.0, 0.1, 4.0, 2.0]
   },
   {
     name: "Doomlings",
-    score: 3.4,
     url: "https://doomlings.com/",
     notes: [
       "Tallying final score is time consuming and could be difficult for some players.",
-      
+
     ],
     youtube: [],
     ratings: [3.2, 3.0, 3.9, 4.1, 2.2, 4.0, 2.0]
   },
   {
     name: "Unstable Unicorns",
-    score: 2.9,
     url: "https://www.unstablegames.com/products/unstable-unicorns?variant=19577791466279",
     notes: [
-      
+
     ],
     youtube: ["https://youtu.be/vt3D41UiGlI?si=CH3hAvtXBURpRzVc"],
     ratings: [2.5, 2.0, 3.0, 4.0, 2.5, 3.5, 2.0]
   },
   {
     name: "One Night Ultimate Werewolf",
-    score: 3.0,
     url: "https://beziergames.com/products/one-night-ultimate-werewolf",
     notes: [
       "Great social deduction game. Similar to the party game and psychology experiment Mafia",
       "I've played the Alien, Daybreak, and Vampire variants. They all are very similar.",
       "Requires at least 4 players.",
-      
+
     ],
     youtube: ["https://youtu.be/XsP6LvZQpLk?si=txkK2mN37srKux5F"],
     ratings: [2.7, 4.0, 4.0, 4.0, 2.0, 1.5, 2.0]
   },
   {
     name: "Battle of the Bards",
-    score: 2.8,
     url: "https://impsandmonsters.com/shop/merch/games/battle-of-the-bards/",
     notes: [
       "Doesn't have expansions, but doesn't necessarily need it.",
-      
+
     ],
     youtube: ["https://youtu.be/YTLKqOQTcY4?si=F4UPGrNZIi6uEf8b"],
     ratings: [3.5, 3.5, 3.2, 3.0, 2.0, 1.5, 2.0]
   },
   {
     name: "Kabuto Sumo",
-    score: 3.1,
     url: "https://www.allplay.com/board-games/kabuto-sumo/",
     notes: [
       "Very little strategizing or planning required.",
       "The most \"physical\" board game I own :D",
-      
+
     ],
     youtube: ["https://youtu.be/eLRy3HHg8vE?si=bG_PKoRBt70arjla"],
     ratings: [2.3, 2.5, 5.0, 4.2, 3.0, 2.0, 3.0]
   },
   {
     name: "Hive",
-    score: 3.7,
     url: "https://www.gen42.com/games/hive",
     notes: [
       "My second favorite road trip game.",
       "I would suggest getting the pocket version, as it is more portable and includes the expansions.",
       "I don't know what the advantage of buying the regular version would be.",
-      
+
     ],
     youtube: ["https://youtu.be/szmcsO9ZCpo?si=bTJ3mtIzB1U1TPb9"],
     ratings: [3.8, 3.8, 4.5, 4.5, 3.0, 3.5, 3.0]
   },
   {
     name: "Happy Little Dinosaurs",
-    score: 2.9,
     url: "https://www.unstablegames.com/products/happy-little-dinosaurs-base-game?variant=19577791500278",
     notes: [
       "I haven't played the expansions, but the base game wasn't attractive enough for me to want to buy more of this game.",
-      
+
     ],
     youtube: ["https://youtu.be/fYzXBvblWtg?si=b6JOYMtRKEu_8aCP"],
     ratings: [2.3, 2.4, 3.4, 4.0, 3.0, 2.5, 2.0]
   },
   {
     name: "Unearth",
-    score: 3.2,
     url: "https://www.brotherwisegames.com/unearth",
     notes: [
-      
+
     ],
     youtube: ["https://youtu.be/J5dGEmh3aaY?si=xwrewJVeqYzVqytI"],
     ratings: [3.3, 4.0, 4.0, 3.3, 1.0, 3.8, 3.0]
   },
   {
     name: "Summoner Wars",
-    score: 3.7,
     url: "https://www.plaidhatgames.com/board-games/summoner-wars/",
     notes: [
       "2 player duel deck builder",
       "I haven't played all the available classes/expansions, but I want to!",
       "There is a digital version. I am unaware if it maintains a player base.",
       "I personally enjoyed the physical version over the digital version.",
-      
+
     ],
     youtube: ["https://youtu.be/_9wYcvNnLPQ?si=XaJQfGA39FDZd5ld"],
     ratings: [3.6, 3.7, 4.0, 3.2, 3.9, 3.8, 4.0]
   },
   {
     name: "Tigris & Euphrates",
-    score: 3.5,
     url: "https://boardgamegeek.com/boardgame/42/tigris-euphrates",
     notes: [
       "I've only played the digital version on BGA",
       "The physical game (or expansions) is currently not in print. There are rumors of a reprint in the near future.",
       "My affordabiity score many rise when the reprint occurs.",
       "I personally don't think the game needs additional expansions.",
-      
+
     ],
     youtube: ["https://youtu.be/p2c7y-AbdOc?si=ZL3EmoVHS3kSICcI"],
     ratings: [4.4, 4.3, 3.2, 3.2, 4.0, 2.0, 2.0]
   },
   {
     name: "Tsuro",
-    score: 3.3,
     url: "https://boardgamegeek.com/boardgame/16992/tsuro",
     notes: [
       "Simple to learn yet rich in replay value, this game captivates players from the very first round."
@@ -243,7 +223,6 @@ export const boardGames = [
   },
   {
     name: "Command of Nature",
-    score: 3.5,
     url: "https://boardgamegeek.com/boardgame/390422/command-of-nature",
     notes: [
       "A deckbuilder that holds its own among the top games in the genre.",
@@ -252,4 +231,35 @@ export const boardGames = [
     youtube: [],
     ratings: [4.2, 3.8, 4.0, 3.8, 3.0, 3.0, 3.0]
   },
+  {
+    name: "Hideous Abominations",
+    url: "https://tettixgames.com/products/hideous-abomination-2nd-edition",
+    notes: [
+      "Tile-laying game for 2-6 people",
+      "Games can be shorter than expected",
+      "Scoring can be tedious",
+      "Quick and easy"
+    ],
+    youtube: [],
+    ratings: [3.0, 2.8, 4.0, 4.0, 2.5, 4.5, 3.8]
+  },
+  {
+    name: "Matcha",
+    url: "https://boardgamegeek.com/boardgame/166107/matcha",
+    notes: [
+      "A light card game for two players that lasts 15-20 minutes",
+      "Can be solved/counted?",
+      "Limited strategies",
+    ],
+    youtube: [],
+    ratings: [3.0, 2.8, 4.0, 4.5, 0, 4.5, 2.0]
+  },
 ];
+
+// Auto-calculate score
+boardGames.forEach(game => {
+  const avg = game.ratings.slice(0, 6).reduce((sum, val) => sum + val, 0) / 6;
+  game.score = Number(avg.toFixed(1));
+});
+
+export { boardGames };
